0c897bb8a0c506ebbd200d45b12bbc840f4549ba
dongjinlang <2837673900@qq.com>
2022-03-05 23:53:17 +0800

    fix: 添加打包分析

--- scripts/config/webpack.prod.js
+++ scripts/config/webpack.prod.js
@@ -1,11 +1,19 @@
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 // eslint-disable-next-line import/extensions
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [new CleanWebpackPlugin()].filter(Boolean),
+  plugins: [
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server', // 开一个本地服务查看报告
+      analyzerHost: '127.0.0.1', // host 设置
+      analyzerPort: 8888, // 端口号设置
+    }),
+  ].filter(Boolean),
   optimization: {
     splitChunks: {
       // include all types of chunks
