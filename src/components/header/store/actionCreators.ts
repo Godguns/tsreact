1946d1329ef4a57b7c229b0e0c05d98ef3a1fb31
dongjinlang <2837673900@qq.com>
2022-03-06 21:26:29 +0800

    fix: 调整接入redux

--- src/components/header/store/actionCreators.ts
+++
@@ -1,6 +0,0 @@
-import * as constants from './constants';
-
-export const setData = (data: any) => ({
-  type: constants.SET_DATA,
-  data,
-});
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import loginReducer from 'Src/pages/login/store/reducer';
-import headerReducer from 'Src/components/header/store/reducer';
+// import headerReducer from 'Src/components/header/store/reducer';
 
 const reducer = combineReducers({
   login: loginReducer,
-  header: headerReducer,
+  // header: headerReducer,
 });
 
 export default reducer;
